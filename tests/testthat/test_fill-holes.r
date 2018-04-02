c4f78de78bf945b80879e304c9a05716bdb20a36
Matt Strimas-Mackey <mstrimas@gmail.com>
2018-04-02 15:03:58 -0400

    wtf

--- tests/testthat/test_fill-holes.r
+++ tests/testthat/test_fill-holes.r
@@ -31,6 +31,7 @@ test_that("fill_holes() works for different input formats", {
   a_diff <- st_area(s_sf) - st_area(s_sfc)
   expect_true(sum(abs(a_diff)) < units::set_units(1, "m^2"))
   a_diff <- st_area(s_sf) - st_area(st_as_sf(s_spdf))
+  stop(a_diff)
   expect_true(sum(abs(a_diff)) < units::set_units(1, "m^2"))
   expect_equivalent(st_set_geometry(s_sf, NULL), s_spdf@data)
 })
